fb19b69291bdebf21313279a296d0b393d3a532d
Chris Dorward <listingslab@gmail.com>
2017-03-03 18:15:05 +1030

    Google maps integration

--- src/containers/Events/Events.jsx
+++ src/containers/Events/Events.jsx
@@ -6,7 +6,7 @@
 
 import React, { Component } from 'react';
 import Loader from '../../components/Loader/Loader';
-import { Panel, Grid, Col, Jumbotron, Row } from 'react-bootstrap';
+import { Panel, Grid, Col, Row } from 'react-bootstrap';
 import './Events.scss';
 
 class Events extends Component {
@@ -25,12 +25,45 @@ class Events extends Component {
     }
   }
 
+  componentDidMount() {
+    if (this.state.eventsLoaded) {
+      this.renderMap();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.eventsLoaded) {
+      this.renderMap();
+    }
+  }
+
   apiEventsCallback(eventsData) {
     cms.eventsData = eventsData;
     this.setState({
       eventsLoaded: true,
       eventsData
     });
+  }
+
+  renderMap() {
+    const map = new google.maps.Map(document.getElementById('event-map'), {});
+    const latlngbounds = new google.maps.LatLngBounds();
+    for (let i = 0; i < this.eventsArr.length; i += 1) {
+      if (this.eventsArr[i].countryIsoCode === 'US') {
+
+        const images = this.eventsArr[i].images || '';
+        const icon = images.icon || '/img/no-image.png';
+        console.log(icon);
+        const place = { lat: this.eventsArr[i].latitude, lng: this.eventsArr[i].longitude };
+        const marker = new google.maps.Marker({
+          position: place,
+          map
+        });
+        latlngbounds.extend(marker.position);
+      }
+    }
+    map.setCenter(latlngbounds.getCenter());
+    map.fitBounds(latlngbounds);
   }
 
   render() {
@@ -42,34 +75,26 @@ class Events extends Component {
         </div>
       );
     } else {
-      const eventsArr = cms.eventsData.contents.data;
-      let eventsPanels = [];
-      // console.log(eventsArr);
-      for (let i = 0; i < eventsArr.length; i += 1) {
-        if (eventsArr[i].countryIsoCode === 'US'){
+      this.eventsArr = cms.eventsData.contents.data;
+      const eventsPanels = [];
+      for (let i = 0; i < this.eventsArr.length; i += 1) {
+        if (this.eventsArr[i].countryIsoCode === 'US') {
           const key = `event_${i}`;
-          eventsPanels.push (
-            <Col
-              key={key}
-              sm={12}
-              md={6}
-            >
-              <Panel
-                className="event-panel"
-              >
-                <h5>{eventsArr[i].name}</h5>
+          eventsPanels.push(
+            <Col key={key} sm={12} md={6}>
+              <Panel className="event-panel">
+                <h5>{this.eventsArr[i].name}</h5>
               </Panel>
             </Col>
           );
         }
-
       }
       content = (
         <div className="container">
           <div className="container">
-            <Jumbotron>
-              <h1>Events</h1>
-            </Jumbotron>
+            <div id="event-map" className="panel">
+              map
+            </div>
           </div>
           <Grid>
             <Row className="show-grid">
